93f6f96bfe3b983b0930e40fa0b52e8b946a687c
dsyrstad <dsyrstad@vscorp.com>
2022-01-10 09:04:53 -0600

    Add support for parsing a list of rfc5322 mailbox-formatted addresses.

--- lib/src/entities/address.dart
+++ lib/src/entities/address.dart
@@ -8,10 +8,92 @@ class Address {
   /// Implementation can override it to pre-process the name before sending.
   /// For example, providing a default name for certain address, or quoting it.
   String? get sanitizedName => name;
+
   /// The address used to output to SMTP server.
   /// Implementation can override it to pre-process the address before sending
   String get sanitizedAddress => mailAddress;
 
   @override
   String toString() => "${name ?? ''} <$mailAddress>";
+}
+
+final _commaCodeUnit = ','.codeUnitAt(0);
+final _quoteCodeUnit = '"'.codeUnitAt(0);
+final _openAngleBracket = '<'.codeUnitAt(0);
+final _closeAngleBracket = '>'.codeUnitAt(0);
+final _backslashCodeUnit = r'\'.codeUnitAt(0);
+
+/// Parse a comma-separated list of rfc5322 3.4 mailboxes to a `List<Address>`.
+/// Does not handle rfc5322 comments.
+List<Address> parseMailboxes(String addresses) {
+  var result = <Address>[];
+  var inQuote = false;
+  var inAngleBrackets = false;
+  var nameOrEmail = <int>[];
+  var email = <int>[];
+  var name = <int>[];
+
+  void addAddress() {
+    if (nameOrEmail.isNotEmpty) {
+      if (email.isEmpty) {
+        email.addAll(nameOrEmail);
+      } else if (name.isEmpty) {
+        name.addAll(nameOrEmail);
+      }
+    }
+
+    if (email.isNotEmpty) {
+      result.add(Address(String.fromCharCodes(email).trim(),
+          String.fromCharCodes(name).trim()));
+    }
+
+    email.clear();
+    name.clear();
+    nameOrEmail.clear();
+    inAngleBrackets = false;
+    inQuote = false;
+  }
+
+  var codeUnits = addresses.codeUnits;
+  for (var p = 0; p < codeUnits.length; p++) {
+    var c = codeUnits[p];
+    if (inQuote) {
+      if (c == _quoteCodeUnit) {
+        inQuote = false;
+      } else if (c == _backslashCodeUnit) {
+        // Handle \" inside of quotes
+        ++p;
+        if (p < codeUnits.length) {
+          var nextC = codeUnits[p];
+          if (nextC == _quoteCodeUnit) {
+            name.add(nextC);
+          } else {
+            --p;
+            name.add(c);
+          }
+        }
+      } else {
+        name.add(c);
+      }
+    } else if (inAngleBrackets) {
+      if (c == _closeAngleBracket) {
+        inAngleBrackets = false;
+      } else {
+        email.add(c);
+      }
+    } else if (c == _commaCodeUnit) {
+      addAddress();
+    } else if (c == _quoteCodeUnit) {
+      inQuote = true;
+    } else if (c == _openAngleBracket) {
+      inAngleBrackets = true;
+    } else {
+      nameOrEmail.add(c);
+    }
+  }
+
+  // Catch last one, if any.
+  addAddress();
+
+  return result;
 }
---
+++ test/parse_mailboxes_test.dart
@@ -0,0 +1,71 @@
+import 'package:test/test.dart';
+import 'package:mailer/mailer.dart';
+
+void main() {
+  final parseMailboxesCases = [
+    {
+      'test': '',
+      'values': <Address>[],
+    },
+    {
+      'test': '  bob@example.com  ',
+      'values': [Address('bob@example.com', '')],
+    },
+    {
+      'test': '  <bob@example.com>  ',
+      'values': [Address('bob@example.com', '')],
+    },
+    {
+      'test': '  Bob Example    <bob@example.com>   ',
+      'values': [Address('bob@example.com', 'Bob Example')],
+    },
+    {
+      'test': ' "  Bob Example  "  <bob@example.com> ',
+      'values': [Address('bob@example.com', 'Bob Example')],
+    },
+    {
+      'test': '"Example, Bob J." <bob@example.com>',
+      'values': [Address('bob@example.com', 'Example, Bob J.')],
+    },
+    {
+      'test': r'"Example, Robert \"Bob\" \J." <bob@example.com>',
+      'values': [Address('bob@example.com', r'Example, Robert "Bob" \J.')],
+    },
+    {
+      'test': 'bob@example.com,jim@example.com',
+      'values': [
+        Address('bob@example.com', ''),
+        Address('jim@example.com', '')
+      ],
+    },
+    {
+      'test': r'''
+        <bob@example.com>, 
+        bob2@example.com, 
+        Bob Example <bob3@example.com>, 
+        "Bob Example" <bob4@example.com>',
+        "Example, Robert \"Bob\" \J." <bob5@example.com>
+        ''',
+      'values': [
+        Address('bob@example.com', ''),
+        Address('bob2@example.com', ''),
+        Address('bob3@example.com', 'Bob Example'),
+        Address('bob4@example.com', 'Bob Example'),
+        Address('bob5@example.com', r'Example, Robert "Bob" \J.'),
+      ],
+    },
+  ];
+
+  for (var t in parseMailboxesCases) {
+    test('parseMailboxes: ${t['test']}', () {
+      final addresses = parseMailboxes(t['test'] as String);
+      final expected = t['values'] as List<Address>;
+      expect(addresses.length, expected.length);
+      for (var i = 0; i < expected.length; i++) {
+        expect(addresses[i].name, expected[i].name, reason: '[$i].name');
+        expect(addresses[i].mailAddress, expected[i].mailAddress,
+            reason: '[$i].mailAddress');
+      }
+    });
+  }
+}
