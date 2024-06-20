5b1f7cf724fa00d9fa5f77efd52b8413374c399e
Christian Loitsch <git@3.141.email>
2024-06-20 09:24:02 +0200

    doc: improve documentation regarding secure connections

--- lib/smtp_server.dart
+++ lib/smtp_server.dart
@@ -10,7 +10,17 @@ class SmtpServer {
   final String host;
   final int port;
   final bool ignoreBadCertificate;
+  /// Connect to the smtp server over a secure ssl connection.
+  /// Setting this option to false does NOT mean, that mails will be sent over
+  /// unencrypted connections!
+  /// SSL for smtp servers is uncommon!
+  /// Usually this library will connect to the server over an insecure
+  /// connection first and use the smtp command `starttls` to upgrade the
+  /// connection to a secure one.  If the server doesn't support
+  /// `starttls` we will abort if `allowInsecure` is false.
   final bool ssl;
+  /// This library will always use secure connections if the server supports it,
+  /// and will abort if unsuccessful unless `allowInsecure` is `true`.
   final bool allowInsecure;
   final String? username;
   final String? password;
