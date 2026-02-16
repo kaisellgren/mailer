aea3abd8fa0806177bb7001c0591ff0ccfa53d10
Christian Loitsch <git@3.141.email>
2026-02-16 08:08:30 +0100

    fix test.dart

--- test/test.dart
+++ test/test.dart
@@ -4,7 +4,7 @@ import 'idna_test.dart' as idna_test;
 import 'message_out_test.dart' as message_out_test;
 import 'punycode_test.dart' as punycode_test;
 import 'rfc3030_test.dart' as rfc3030_test;
-import 'send_test.dart' as send_test;
+import 'smtp_client_test.dart' as client_test;
 import 'split_test.dart' as split_test;
 import 'stream_splitter_test.dart' as stream_splitter_test;
 import 'validator_test.dart' as validator_test;
@@ -18,7 +18,7 @@ void main() {
   group('message_out_test', message_out_test.main);
   group('punycode_test', punycode_test.main);
   group('rfc3030_test', rfc3030_test.main);
-  group('send_test', send_test.main);
+  group('client_test', client_test.main);
   group('split_test', split_test.main);
   group('stream_splitter_test', stream_splitter_test.main);
   group('validator_test', validator_test.main);
